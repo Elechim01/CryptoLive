51e0751c680309d8257c8ab5c6eaf2fd3bcc1f98
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-11-26 22:46:18 +0100

    Initial Commit

---
+++ CryptoLive/CryptoLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoLiveApp.swift
+//  CryptoLive
+//
+//  Created by Michele Manniello on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
